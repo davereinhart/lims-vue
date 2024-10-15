71cdfc2cce380427eabf67d33593d639eb7f17c7
David Reinhart <davidrr@uw.edu>
2024-10-15 12:54:48 -0700

    update URLs to reflect API endpoint changes

--- src/service/SpecimenService.js
+++ src/service/SpecimenService.js
@@ -3,13 +3,13 @@ const baseUrl = `${import.meta.env.VITE_API_URL}/api/specimen`
 
 export const SpecimenService = {
     async getSpecimensData() {
-        const specimens = await axios.get(`${baseUrl}/all`)
+        const specimens = await axios.get(`${baseUrl}`)
         return specimens
     },
 
     async updateSpecimensData(records) {
         if (records.length == 1) {
-            const updatedRecords = await axios.put(`${baseUrl}/update`, records[0])
+            const updatedRecords = await axios.put(`${baseUrl}/${records[0].id}`, records[0])
             return updatedRecords
         }
     }
